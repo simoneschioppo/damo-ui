c6ec52a7cf952c6000f2808d90baa4cab6cbcd13
Simone Schioppo <simone.schioppo94@gmail.com>
2026-04-18 19:16:36 +0200

    chore: setup prettier + eslint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/**', '**/.next/**', '**/node_modules/**', '**/coverage/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+)
