d81b3e784d2b81c03926de49e988afaf83524794
Simone Schioppo <simone.schioppo94@gmail.com>
2026-04-18 21:18:09 +0200

    feat(ui): add AlertDialog (Radix + default/danger tone)

---
+++ packages/ui/src/components/alert-dialog/index.ts
@@ -0,0 +1,14 @@
+export {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+  type AlertDialogContentProps,
+} from './alert-dialog'
