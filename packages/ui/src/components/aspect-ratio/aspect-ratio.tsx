e1900a7f227ac6c354f2fe0b0a9b1f8a52e24da8
Simone Schioppo <simone.schioppo94@gmail.com>
2026-04-18 20:41:36 +0200

    feat(ui): add AspectRatio component (Radix)

---
+++ packages/ui/src/components/aspect-ratio/aspect-ratio.tsx
@@ -0,0 +1,11 @@
+import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio'
+import { forwardRef, type ComponentPropsWithoutRef } from 'react'
+
+export type AspectRatioProps = ComponentPropsWithoutRef<typeof AspectRatioPrimitive.Root>
+
+export const AspectRatio = forwardRef<HTMLDivElement, AspectRatioProps>(function AspectRatio(
+  props,
+  ref,
+) {
+  return <AspectRatioPrimitive.Root ref={ref} {...props} />
+})
