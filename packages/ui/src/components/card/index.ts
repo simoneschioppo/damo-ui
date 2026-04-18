34bb20e3dd9fad467670b6063492a6db948b249e
Simone Schioppo <simone.schioppo94@gmail.com>
2026-04-18 21:10:04 +0200

    feat(ui): add Card (5 variants + header/body/footer subparts)

---
+++ packages/ui/src/components/card/index.ts
@@ -0,0 +1,10 @@
+export {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardBody,
+  CardFooter,
+  type CardProps,
+} from './card'
+export { cardVariants, type CardVariants } from './card.variants'
